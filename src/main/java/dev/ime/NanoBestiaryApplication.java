ad63b2901e62297c7088eb8f4c27206bb3ef8afc
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-11-25 07:59:44 +0100

    feat: initial commit

---
+++ src/main/java/dev/ime/NanoBestiaryApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoBestiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoBestiaryApplication.class, args);
+	}
+
+}
