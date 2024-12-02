c59d7172b237c5af7feecff621a2f3e9a0af0f25
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-02 07:40:24 +0100

    feat: add initial commit ms-creature

---
+++ ms-creature/src/main/java/dev/ime/MsCreatureApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCreatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCreatureApplication.class, args);
+	}
+
+}
