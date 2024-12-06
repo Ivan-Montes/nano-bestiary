ef886633bef2861089247d1dcbf84e548a451cad
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-06 07:31:51 +0100

    feat: add Kafka config

---
+++ ms-area/src/main/java/dev/ime/config/KafkaConfig.java
@@ -0,0 +1,41 @@
+package dev.ime.config;
+
+import java.util.Map;
+
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.boot.ssl.SslBundles;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+import org.springframework.kafka.core.reactive.ReactiveKafkaProducerTemplate;
+
+import reactor.kafka.sender.SenderOptions;
+
+@Configuration
+public class KafkaConfig {
+
+	@Bean
+	KafkaAdmin.NewTopics topics() {
+		
+		return new NewTopics(
+				TopicBuilder.name(GlobalConstants.AREA_CREATED).build(),
+				TopicBuilder.name(GlobalConstants.AREA_UPDATED).build(),
+				TopicBuilder.name(GlobalConstants.AREA_DELETED).build()
+				);
+		
+	}
+	
+    @Bean
+    ReactiveKafkaProducerTemplate<String, Object> reactiveKafkaProducerTemplate(
+            KafkaProperties kafkaProperties,
+            SslBundles sslBundles) {
+        
+        Map<String, Object> props = kafkaProperties.buildProducerProperties(sslBundles);
+       
+        return new ReactiveKafkaProducerTemplate<>(SenderOptions.create(props));
+        
+    }
+    
+}
