0cd13dcd1f8dc3283956f5c08bcb75f57a23f537
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-12-12 09:07:41 +0100

    feat: add RSocket service layer

---
+++ ms-area/src/main/java/dev/ime/application/service/RSocketService.java
@@ -0,0 +1,30 @@
+package dev.ime.application.service;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Service;
+
+import dev.ime.domain.ports.inbound.RSocketServicePort;
+import dev.ime.domain.ports.outbound.RSocketRepositoryPort;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class RSocketService implements RSocketServicePort{
+
+	private final RSocketRepositoryPort rSocketRepositoryPort;
+
+	public RSocketService(RSocketRepositoryPort rSocketRepositoryPort) {
+		super();
+		this.rSocketRepositoryPort = rSocketRepositoryPort;
+	}
+
+	@Override
+	public Mono<Boolean> existsById(UUID id) {
+		
+		return rSocketRepositoryPort
+				.existsById(id);
+		
+	}
+	
+}
