36932b35e6f4615785eff8833bba1db934835304
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2025-01-10 07:37:46 +0100

    feat: add Logger utils

---
+++ ms-creature/src/main/java/dev/ime/application/utils/ReactiveLoggerUtils.java
@@ -0,0 +1,50 @@
+package dev.ime.application.utils;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Component;
+
+import dev.ime.config.GlobalConstants;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+@Component
+public class ReactiveLoggerUtils {
+
+	private final Logger logger;
+
+	public ReactiveLoggerUtils(Logger logger) {
+		super();
+		this.logger = logger;
+	}
+	
+	public Mono<Void> logSevereAction(String msg) {
+		
+		return Mono.fromRunnable( () -> 
+			logger.log(
+					Level.SEVERE,
+					String.format(GlobalConstants.MSG_PATTERN_SEVERE, msg)
+					)
+			)
+			.subscribeOn(Schedulers.boundedElastic())
+			.onErrorResume( e -> Mono.empty())
+			.then();
+		
+	} 
+
+	public Mono<Void> logInfoAction(String className, String methodName, String msg) {
+				
+		return Mono.fromRunnable( () -> 
+			logger.log(
+					Level.INFO, 
+					String.format(GlobalConstants.MSG_PATTERN_INFO, className, methodName, msg)
+					)
+			)
+			.subscribeOn(Schedulers.boundedElastic())
+			.onErrorResume( e -> Mono.empty())
+			.then();
+		
+	}
+	
+}
